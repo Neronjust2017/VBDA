c1ce0a589fe842dafba8658c4bea892cb33b2191
Neronjust2017 <1036758468@qq.com>
2022-06-02 21:17:48 +0800

    first commit

---
+++ vbda.py
@@ -0,0 +1,353 @@
+"""
+@author: Junguang Jiang
+@contact: JiangJunguang1123@outlook.com
+"""
+import random
+import time
+import warnings
+import sys
+import argparse
+import shutil
+import os.path as osp
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+sys.path.append('../../..')
+
+from common.utils.data import ForeverDataIterator
+from common.utils.metric import accuracy
+from common.utils.meter import AverageMeter, ProgressMeter
+from common.utils.logger import CompleteLogger
+from common.utils.analysis import collect_feature, tsne, a_distance
+
+from modules.kernels import GaussianKernel
+from modules.vbda import MultipleKernelMaximumMeanDiscrepancy, ImageClassifier
+
+sys.path.append('.')
+import utils
+import metrics
+import config_bayesian as cfg
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def main(args: argparse.Namespace):
+    logger = CompleteLogger(args.log, args.phase)
+    print(args)
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    train_transform = utils.get_train_transform(args.train_resizing, random_horizontal_flip=not args.no_hflip,
+                                                random_color_jitter=False, resize_size=args.resize_size,
+                                                norm_mean=args.norm_mean, norm_std=args.norm_std)
+    val_transform = utils.get_val_transform(args.val_resizing, resize_size=args.resize_size,
+                                            norm_mean=args.norm_mean, norm_std=args.norm_std)
+    print("train_transform: ", train_transform)
+    print("val_transform: ", val_transform)
+
+    train_source_dataset, train_target_dataset, val_dataset, test_dataset, num_classes, args.class_names = \
+        utils.get_dataset(args.data, args.root, args.source, args.target, train_transform, val_transform)
+    train_source_loader = DataLoader(train_source_dataset, batch_size=args.batch_size,
+                                     shuffle=True, num_workers=args.workers, drop_last=True)
+    train_target_loader = DataLoader(train_target_dataset, batch_size=args.batch_size,
+                                     shuffle=True, num_workers=args.workers, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+
+    train_source_iter = ForeverDataIterator(train_source_loader)
+    train_target_iter = ForeverDataIterator(train_target_loader)
+    
+    # load config parameters
+    priors = cfg.priors
+    train_ens = cfg.train_ens
+    valid_ens = cfg.valid_ens
+    
+    # create model
+    print("=> using model '{}'".format(args.arch))
+    backbone = utils.get_model_vbda(args.arch, priors=priors)
+    pool_layer = nn.Identity() if args.no_pool else None
+    classifier = ImageClassifier(backbone, num_classes, bottleneck_dim=args.bottleneck_dim,
+                                 pool_layer=pool_layer, finetune=not args.scratch).to(device)
+
+    # define optimizer and lr scheduler
+    optimizer = SGD(classifier.get_parameters(), args.lr, momentum=args.momentum, weight_decay=args.wd, nesterov=True)
+    lr_scheduler = LambdaLR(optimizer, lambda x:  args.lr * (1. + args.lr_gamma * float(x)) ** (-args.lr_decay))
+
+    # define loss function
+    mkmmd_loss = MultipleKernelMaximumMeanDiscrepancy(
+        kernels=[GaussianKernel(alpha=2 ** k) for k in range(-3, 2)],
+        linear=not args.non_linear
+    )
+
+    # resume from the best checkpoint
+    if args.phase != 'train':
+        checkpoint = torch.load(logger.get_checkpoint_path('best'), map_location='cpu')
+        classifier.load_state_dict(checkpoint)
+
+    # analysis the model
+    if args.phase == 'analysis':
+        # extract features from both domains
+        feature_extractor = nn.Sequential(classifier.backbone, classifier.pool_layer, classifier.bottleneck).to(device)
+        source_feature = collect_feature(train_source_loader, feature_extractor, device)
+        target_feature = collect_feature(train_target_loader, feature_extractor, device)
+        # plot t-SNE
+        tSNE_filename = osp.join(logger.visualize_directory, 'TSNE.pdf')
+        tsne.visualize(source_feature, target_feature, tSNE_filename)
+        print("Saving t-SNE to", tSNE_filename)
+        # calculate A-distance, which is a measure for distribution discrepancy
+        A_distance = a_distance.calculate(source_feature, target_feature, device)
+        print("A-distance =", A_distance)
+        return
+
+    # if args.phase == 'test':
+    #     acc1 = validate(test_loader, classifier, args, device)
+    #     print(acc1)
+    #     return
+
+    # start training
+    best_acc1 = 0.
+    for epoch in range(args.epochs):
+        # train for one epoch
+        train(train_source_iter, train_target_iter, classifier, mkmmd_loss, optimizer,
+              lr_scheduler, epoch, train_ens, args)
+
+        # evaluate on validation set
+        acc1 = validate(val_loader, classifier, valid_ens, args, device)
+
+        # remember best acc@1 and save checkpoint
+        torch.save(classifier.state_dict(), logger.get_checkpoint_path('latest'))
+        if acc1 > best_acc1:
+            shutil.copy(logger.get_checkpoint_path('latest'), logger.get_checkpoint_path('best'))
+        best_acc1 = max(acc1, best_acc1)
+
+    print("best_acc1 = {:3.1f}".format(best_acc1))
+
+    # evaluate on test set
+    classifier.load_state_dict(torch.load(logger.get_checkpoint_path('best')))
+    acc1 = validate(test_loader, classifier, valid_ens, args, device)
+    print("test_acc1 = {:3.1f}".format(acc1))
+
+    logger.close()
+
+
+def train(train_source_iter: ForeverDataIterator, train_target_iter: ForeverDataIterator, model: ImageClassifier,
+          mkmmd_loss: MultipleKernelMaximumMeanDiscrepancy, optimizer: SGD,
+          lr_scheduler: LambdaLR, epoch: int, num_ens: int, args: argparse.Namespace):
+    batch_time = AverageMeter('Time', ':4.2f')
+    data_time = AverageMeter('Data', ':3.1f')
+    losses = AverageMeter('Loss', ':3.2f')
+    trans_losses = AverageMeter('Trans Loss', ':5.4f')
+    kl_losses = AverageMeter('KL Loss', ':5.4f')
+    cls_accs = AverageMeter('Cls Acc', ':3.1f')
+    tgt_accs = AverageMeter('Tgt Acc', ':3.1f')
+
+    progress = ProgressMeter(
+        args.iters_per_epoch,
+        [batch_time, data_time, losses, trans_losses, cls_accs, tgt_accs],
+        prefix="Epoch: [{}]".format(epoch))
+
+    # switch to train mode
+    model.train()
+    mkmmd_loss.train()
+
+    end = time.time()
+    for i in range(args.iters_per_epoch):
+        x_s, labels_s = next(train_source_iter)
+        x_t, labels_t = next(train_target_iter)
+
+        x_s = x_s.to(device)
+        x_t = x_t.to(device)
+        labels_s = labels_s.to(device)
+        labels_t = labels_t.to(device)
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        # compute output
+        ys_s = torch.zeros(x_s.shape[0], model.num_classes, num_ens).to(device)
+        ys_t = torch.zeros(x_t.shape[0], model.num_classes, num_ens).to(device)
+        fs_s = torch.zeros(x_s.shape[0], model.features_dim, num_ens).to(device)
+        fs_t = torch.zeros(x_t.shape[0], model.features_dim, num_ens).to(device)
+
+        kl_loss = 0.0
+
+        for j in range(num_ens):
+
+            y_s, f_s, _kl = model(x_s)
+            y_t, f_t, _ = model(x_t)
+
+            ys_s[:,:,j] = F.log_softmax(y_s, dim=1)
+            ys_t[:,:,j] = F.log_softmax(y_t, dim=1)
+            fs_s[:,:,j] = f_s
+            fs_t[:,:,j] = f_t
+
+            kl_loss = kl_loss + _kl
+
+        ys_s = utils.logmeanexp(ys_s, dim=2)
+        ys_t = utils.logmeanexp(ys_t, dim=2)
+        fs_s = torch.mean(fs_s, dim=2)
+        fs_t = torch.mean(fs_t, dim=2)
+
+        cls_loss = F.nll_loss(ys_s, labels_s, reduction='mean')
+        transfer_loss = mkmmd_loss(fs_s, fs_t)
+        kl_loss = kl_loss / num_ens
+
+        # beta
+
+        loss = cls_loss + transfer_loss * args.alph + kl_loss * args.beta
+
+        cls_acc = accuracy(torch.exp(ys_s), labels_s)[0]
+        tgt_acc = accuracy(torch.exp(ys_t), labels_t)[0]
+
+        losses.update(loss.item(), x_s.size(0))
+        cls_accs.update(cls_acc.item(), x_s.size(0))
+        tgt_accs.update(tgt_acc.item(), x_t.size(0))
+        trans_losses.update(transfer_loss.item(), x_s.size(0))
+        kl_losses.update(kl_loss.item())
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        lr_scheduler.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.display(i)
+
+def validate(val_loader, model, num_ens, args, device) -> float:
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    progress = ProgressMeter(
+        len(val_loader),
+        [batch_time, losses, top1],
+        prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+
+            images = images.to(device)
+            target = target.to(device)
+
+            # compute output
+            outputs = torch.zeros(images.shape[0], model.num_classes, num_ens).to(device)
+
+            for j in range(num_ens):
+                output, _, _ = model(images)
+                outputs[:, :, j] = F.log_softmax(output, dim=1).data
+
+            outputs = utils.logmeanexp(outputs, dim=2)
+            loss = F.nll_loss(outputs, target)
+
+            # measure accuracy and record loss
+            acc1, = accuracy(torch.exp(outputs), target, topk=(1,))
+
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1.item(), images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+        print(' * Acc@1 {top1.avg:.3f}'.format(top1=top1))
+
+
+    return top1.avg
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DAN for Unsupervised Domain Adaptation')
+    # dataset parameters
+    parser.add_argument('root', metavar='DIR',
+                        help='root path of dataset')
+    parser.add_argument('-d', '--data', metavar='DATA', default='Office31', choices=utils.get_dataset_names(),
+                        help='dataset: ' + ' | '.join(utils.get_dataset_names()) +
+                             ' (default: Office31)')
+    parser.add_argument('-s', '--source', help='source domain(s)', nargs='+')
+    parser.add_argument('-t', '--target', help='target domain(s)', nargs='+')
+    parser.add_argument('--train-resizing', type=str, default='default')
+    parser.add_argument('--val-resizing', type=str, default='default')
+    parser.add_argument('--resize-size', type=int, default=224,
+                        help='the image size after resizing')
+    parser.add_argument('--no-hflip', action='store_true',
+                        help='no random horizontal flipping during training')
+    parser.add_argument('--norm-mean', type=float, nargs='+',
+                        default=(0.485, 0.456, 0.406), help='normalization mean')
+    parser.add_argument('--norm-std', type=float, nargs='+',
+                        default=(0.229, 0.224, 0.225), help='normalization std')
+    # model parameters
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                        choices=utils.get_model_names(),
+                        help='backbone architecture: ' +
+                             ' | '.join(utils.get_model_names()) +
+                             ' (default: resnet18)')
+    parser.add_argument('--bottleneck-dim', default=256, type=int,
+                        help='Dimension of bottleneck')
+    parser.add_argument('--no-pool', action='store_true',
+                        help='no pool layer after the feature extractor.')
+    parser.add_argument('--scratch', action='store_true', help='whether train from scratch.')
+    parser.add_argument('--non-linear', default=False, action='store_true',
+                        help='whether not use the linear version')
+    parser.add_argument('--alph', default=1., type=float,
+                        help='the trade-off hyper-parameter for transfer loss')
+    parser.add_argument('--beta', default=1., type=float,
+                        help='the trade-off hyper-parameter for kl loss')
+    # training parameters
+    parser.add_argument('-b', '--batch-size', default=32, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 32)')
+    parser.add_argument('--lr', '--learning-rate', default=0.003, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--lr-gamma', default=0.0003, type=float, help='parameter for lr scheduler')
+    parser.add_argument('--lr-decay', default=0.75, type=float, help='parameter for lr scheduler')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=0.0005, type=float,
+                        metavar='W', help='weight decay (default: 5e-4)')
+    parser.add_argument('-j', '--workers', default=2, type=int, metavar='N',
+                        help='number of data loading workers (default: 2)')
+    parser.add_argument('--epochs', default=20, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('-i', '--iters-per-epoch', default=500, type=int,
+                        help='Number of iterations per epoch')
+    parser.add_argument('-p', '--print-freq', default=100, type=int,
+                        metavar='N', help='print frequency (default: 100)')
+    parser.add_argument('--seed', default=None, type=int,
+                        help='seed for initializing training. ')
+    parser.add_argument('--per-class-eval', action='store_true',
+                        help='whether output per-class accuracy during evaluation')
+    parser.add_argument("--log", type=str, default='dan',
+                        help="Where to save logs, checkpoints and debugging images.")
+    parser.add_argument("--phase", type=str, default='train', choices=['train', 'test', 'analysis'],
+                        help="When phase is 'test', only test the model."
+                             "When phase is 'analysis', only analysis the model.")
+    args = parser.parse_args()
+    main(args)
+
